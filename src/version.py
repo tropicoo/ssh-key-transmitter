9316f5732c82ba7cf713cab812fcb2e4c3171287
tropicoo <888784+tropicoo@users.noreply.github.com>
2026-01-15 23:53:12 +0200

    [0.2.4] Maintenance

--- src/version.py
+++ src/version.py
@@ -1,3 +1,3 @@
 from typing import Final
 
-__version__: Final[str] = '0.2.3'
+__version__: Final[str] = '0.2.4'
--- ssh-key-transmit.py
+++ ssh-key-transmit.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 """SSH Key Transmitter Module."""
 
 import typer
